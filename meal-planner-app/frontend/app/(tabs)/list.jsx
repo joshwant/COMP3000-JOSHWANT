65739cde864ff914683f735c555af1c2321cb9e8
joshwant <joshua.want@students.plymouth.ac.uk>
2024-12-11 01:05:08 +0000

    Updated Nav Bar

--- meal-planner-app/frontend/app/(tabs)/list.jsx
+++ meal-planner-app/frontend/app/(tabs)/list.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const list = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>List</Text>
+      <Text style={styles.text}>Shopping List</Text>
     </View>
   )
 }
