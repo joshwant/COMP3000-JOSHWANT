0652e1f7e27bba23d4f0d88b4a644517fadb3663
joshwant <joshua.want@students.plymouth.ac.uk>
2025-03-12 23:40:33 +0000

    Fixed issue where the app wont run on IOS device. Also fixed some errors with incompatible packages.

--- meal-planner-app/frontend/app.config.js
+++ meal-planner-app/frontend/app.config.js
@@ -18,6 +18,7 @@ export default {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#ffffff",
       },
+      package: "com.anonymous.frontend"
     },
     web: {
       bundler: "metro",
