0415937a3d678d95193f192d1bc074f44252e778
joshwant <joshua.want@students.plymouth.ac.uk>
2025-03-13 00:22:25 +0000

    Trying to fix Text error when loading app on physical device

--- meal-planner-app/frontend/app/auth/welcome.jsx
+++ meal-planner-app/frontend/app/auth/welcome.jsx
@@ -10,9 +10,9 @@ const Welcome = () => {
       <Text style={styles.text}>Welcome</Text>
 
       <Pressable style={styles.button} onPress={() => navigation.navigate('signup')}>
-              <Text style={styles.buttonText}>Sign Up</Text>
-            </Pressable>
-            
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </Pressable>
+
       <Pressable style={styles.button} onPress={() => navigation.navigate('login')}>
         <Text style={styles.buttonText}>Login</Text>
       </Pressable>
@@ -20,45 +20,36 @@ const Welcome = () => {
   );
 };
 
-export default Welcome;
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      backgroundColor: 'white',
-    },
-    text: {
-      color: 'white',
-      fontSize: 42,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      backgroundColor: 'rgba(0,0,0,0.5)',
-      marginBottom: 120,
-      marginTop: 40,
-    },
-    link: {
-      color: 'white',
-      fontSize: 42,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      textDecorationLine: 'underline',
-      backgroundColor: 'rgba(0,0,0,0.5)',
-      padding: 4,
-    },
-    button: {
-      height: 60,
-      borderRadius: 20,
-      justifyContent: 'center',
-      backgroundColor: 'rgba(0,0,0,0.75)',
-      padding: 6,
-      marginBottom: 20,
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 16,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      padding: 4,
-    }
-  })
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    marginBottom: 120,
+    marginTop: 40,
+  },
+  button: {
+    height: 60,
+    borderRadius: 20,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.75)',
+    padding: 6,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 4,
+  }
+});
+
+export default Welcome;
