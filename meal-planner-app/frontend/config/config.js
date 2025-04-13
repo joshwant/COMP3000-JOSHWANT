98f21e17e08218da0f9dad7bfc85771bbebb9e11
joshwant <joshua.want@students.plymouth.ac.uk>
2025-04-13 23:35:05 +0100

    Improved Switch Item Modal - Also fixing the config to work from different locations

--- meal-planner-app/frontend/config/config.js
+++ meal-planner-app/frontend/config/config.js
@@ -1,1 +1,15 @@
-export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.0.173:5000';
+//Chedworth:
+//export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.0.173:5000';
+
+//Worcs:
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.1.152:5000';
+
+//Uni:
+
+
+//How to find:
+//cmd
+//'ipconfig'
+//then the IPv4 Address
+
+//Do this and .env
